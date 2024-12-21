18353fcba92770f5ccb65d770d59acdc7aebddd6
irfantkd <167754011+irfantkd@users.noreply.github.com>
2024-12-21 13:16:12 +0500

    working on the session storage of hte number and send otp adn varify otp and reset password

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
+
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 const UserRouts = require("./routes/User.routes");
 const CarRouts = require("./routes/Car.routes");
 const BranchRouts = require("./routes/Branch.routes");
+const crypto = require("crypto");
 
 const port = 3003; // Define the port
 
@@ -13,6 +16,21 @@ const port = 3003; // Define the port
 app.use(express.json());
 app.use(cors());
 
+const secret = crypto.randomBytes(32).toString("hex");
+console.log(secret);
+
+app.use(
+  session({
+    secret: secret || "your-secret-key", // Use environment variable for security
+    resave: false, // Prevent resaving sessions if unchanged
+    saveUninitialized: false, // Don't save empty sessions
+    cookie: {
+      httpOnly: true, // Prevent client-side JavaScript from accessing cookies
+      secure: false, // Set to true if using HTTPS
+      maxAge: 30 * 60 * 1000, // Session expiration (30 minutes)
+    },
+  })
+);
 // Routes
 app.get("/", (req, res) => {
   res.send("Hello, world!");
